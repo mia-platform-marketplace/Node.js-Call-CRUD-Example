e45bdfd8153831ef7ea7a7f942a6146cd15fb468
Filippo Rezzonico <filippo1.rezzonico@mail.polimi.it>
2020-05-18 17:37:19 +0200

    protocol change

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ const customService = require('@mia-platform/custom-plugin-lib')({
 
 module.exports = customService(async function index(service) {
   service.addRawCustomPlugin('GET', '/riders', async(request, reply) => {
-    const proxy = request.getDirectServiceProxy(service.config.CRUD_PATH, { protocol: 'https' })
+    const proxy = request.getDirectServiceProxy(service.config.CRUD_PATH, { protocol: 'http' })
     const { statusCode, payload } = await proxy.get('/v2/riders/')
     reply.code(statusCode).send(payload)
   })
   service.addRawCustomPlugin('GET', '/riders/:id', async(request, reply) => {
     const { id } = request.params
-    const proxy = request.getDirectServiceProxy(service.config.CRUD_PATH, { protocol: 'https' })
+    const proxy = request.getDirectServiceProxy(service.config.CRUD_PATH, { protocol: 'http' })
     const { statusCode, payload } = await proxy.get(`/v2/riders/${id}`)
     reply.code(statusCode).send(payload)
   })
